c3988194ea2d3c694e8f964ef373a3e71a900cd4
Pallavi Khadse <117720494+Pallavi687@users.noreply.github.com>
2023-03-22 16:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function Hello(){
+	return new Promise((resolve,reject)=>{
+		serTimeout(()=>{
+			resolve("Hello, world!");
+		},1000);
+	});
+	Hello().then(data=>{
+		document.getElementById("output").textContent=data;
+	});
+}
